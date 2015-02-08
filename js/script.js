316fef0de5bf0d2042f51ec16e4800e44cc169b2
William Mahoney <wm.j.mahoney@gmail.com>
2015-02-08 11:06:57 -0500

    added some more javascript to calculate scores

---
+++ js/script.js
@@ -0,0 +1,59 @@
+function submitAnswers() {
+  var total = 5;
+  var score = 0;
+
+  //..Get User Input
+  var q1 = document.forms["quizForm"]["q1"].value;
+  var q2 = document.forms["quizForm"]["q2"].value;
+  var q3 = document.forms["quizForm"]["q3"].value;
+  var q4 = document.forms["quizForm"]["q4"].value;
+  var q5 = document.forms["quizForm"]["q5"].value;
+
+
+  // Validation
+  for(i = 1; i <= total; i++){
+    if(eval('q'+i) == null || eval('q'+i) == '') {
+      alert('You missed question ' + i);
+      return false;
+    }
+  }
+
+  // return false at the end of function stops the form from being submitted elsewhere
+
+
+  // Set Correct Answers
+  var answers = ["b","a","d","b","d"];
+
+  // Check Answers
+
+
+  for(i = 1; i <= total; i++){
+    if(eval('q' + i) == answers[i - 1]) {
+      debugger
+      score++;
+    }
+  }
+
+  //
+  // if(q1 == answers[0]) {
+  //   score++;
+  // }
+  //
+  // if(q2 == answers[1]) {
+  //   score++;
+  // }
+  //
+  // if(q3 == answers[2]) {
+  //   score++;
+  // }
+  //
+  // if(q4 == answers[3]) {
+  //   score++;
+  // }
+  //
+  // if(q5 == answers[4]) {
+  //   score++;
+  // }
+
+  alert("You scored " + score + " out of " + total);
+}
